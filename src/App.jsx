ba37b0ae49ce69384faf4ecb9e059ba3e2fedee8
Felipe segura <yfsegura04@misena.edu.co>
2023-02-26 16:35:21 -0500

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-
 import { ServicesContextProvider } from "./Context/Context";
 import AppRouter from "./Routes/AppRouter";
 
 function App() {
-  
-
-  
-  
   return (
     <>
       <ServicesContextProvider>
-        
-        <AppRouter/>
+        <AppRouter />
       </ServicesContextProvider>
     </>
   );
--- src/Routes/AppRouter.jsx
+++ src/Routes/AppRouter.jsx
@@ -1,13 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import { Home, About, Contact, Header } from "../Components";
 import Navbar from "../dashboard/Menus/Navbar";
-import {Conductores,DatosCliente,DatosClienteEmpresa,DatosConductores,Historial,Login,Pagina404,Solicitudes,Pqrs} from '../Pages'
-
-
-
+import {
+  Conductores,
+  DatosCliente,
+  DatosClienteEmpresa,
+  DatosConductores,
+  Historial,
+  Login,
+  Pagina404,
+  Solicitudes,
+  Pqrs,
+} from "../Pages";
 
 export const AppRouter = () => {
-  
   return (
     <>
       {/* PUBLICAS*/}
@@ -19,49 +25,6 @@ export const AppRouter = () => {
         </Route>
         <Route path="Login" element={<Login />} />
         <Route path="404" element={<Pagina404 />} />
-        {/* <Route path="/dashboard" element={<Navbar />} />
-          <PrivateRoute
-            path="/"
-            element={<Conductores />}
-            isAuthenticated={isAuthenticated}
-            token={token}
-          />
-          <PrivateRoute
-            path="solicitudes"
-            element={<Solicitudes />}
-            isAuthenticated={isAuthenticated}
-            token={token}
-          />
-          <PrivateRoute
-            path="datosconductores"
-            element={<DatosConductores />}
-            isAuthenticated={isAuthenticated}
-            token={token}
-          />
-          <PrivateRoute
-            path="datoscliente"
-            element={<DatosCliente />}
-            isAuthenticated={isAuthenticated}
-            token={token}
-          />
-          <PrivateRoute
-            path="datosclienteEmpresa"
-            element={<DatosClienteEmpresa />}
-            isAuthenticated={isAuthenticated}
-            token={token}
-          />
-          <PrivateRoute
-            path="historial"
-            element={<Historial />}
-            isAuthenticated={isAuthenticated}
-            token={token}
-          />
-          <PrivateRoute
-            path="pqrs"
-            element={<Pqrs />}
-            isAuthenticated={isAuthenticated}
-            token={token}
-          /> */}
         {/* PRIVADAS*/}*
         <Route path="/dashboard" element={<Navbar />}>
           <Route index element={<Conductores />} />
@@ -73,32 +36,6 @@ export const AppRouter = () => {
           <Route path="pqrs" element={<Pqrs />} />
         </Route>
         <Route path="/pagina404" element={<Pagina404 />} />
-        {/* <Route path="login" element={<Login setIsAuthenticated={setIsAuthenticated} />} >
-        <PrivateRoute
-          path="/dashboard"
-          element={<Navbar />}
-          isAuthenticated={isAuthenticated}
-        >
-          <Route index element={<Conductores />} />
-          <Route path="solicitudes" element={<Solicitudes />} />
-          <Route path="datosconductores" element={<DatosConductores />} />
-          <Route path="datoscliente" element={<DatosCliente />} />
-          <Route path="datosclienteEmpresa" element={<DatosClienteEmpresa />} />
-          <Route path="historial" element={<Historial />} />
-          <Route path="pqrs" element={<Pqrs />} />
-        </PrivateRoute>
-           
-        </Route> */}
-        {/* <PrivateRoute isAuthenticated={isAuthenticated} path="/dashboard" element={<Navbar />}>
-          <Route index element={<Conductores />} />
-          <PrivateRoute isAuthenticated={isAuthenticated} path="solicitudes" element={<Solicitudes />} />
-          <PrivateRoute isAuthenticated={isAuthenticated} path="datosconductores" element={<DatosConductores />} />
-          <PrivateRoute isAuthenticated={isAuthenticated} path="datoscliente" element={<DatosCliente />} />
-          <PrivateRoute isAuthenticated={isAuthenticated} path="datosclienteEmpresa" element={<DatosClienteEmpresa />} />
-          <PrivateRoute isAuthenticated={isAuthenticated} path="historial" element={<Historial />} />
-          <PrivateRoute isAuthenticated={isAuthenticated} path="pqrs" element={<Pqrs />} />
-        </PrivateRoute> */}
-        {/* ruta por defecto */}
       </Routes>
     </>
   );
